19110ac73385a99005939c5e1390bb4cf7856427
Anthony Lowhur <antlowhur@yahoo.com>
2015-10-29 19:24:07 -0400

    Added more image data

--- train_and_predict.py
+++ train_and_predict.py
@@ -4,8 +4,9 @@ import cv2
 from matplotlib import pyplot as plt
 import imutils
 from pyimagesearch import imutilspy
-import numpy as np
+from time import time
 import os
+
 from sklearn.svm import LinearSVC
 from sklearn.externals import joblib
 from scipy.cluster.vq import *
@@ -14,6 +15,120 @@ from sklearn.preprocessing import StandardScaler
 from sklearn import cross_validation
 from sklearn.metrics import accuracy_score,accuracy_score, confusion_matrix
 
+from sklearn.linear_model import Ridge
+from sklearn.learning_curve import validation_curve, learning_curve
+from sklearn.svm import SVC
+from sklearn import svm
+
+from sklearn.cross_validation import ShuffleSplit
+from sklearn.grid_search import GridSearchCV
+
+
+def main_mat_construct(image_paths_m):
+    print 'Iterating through features. . .'
+    for image_path in image_paths_m:
+        #print image_path
+        im = cv2.imread(image_path)
+        im = imutilspy.resize(im, height = 200)
+        
+        #kpts = fea_det.detect(im)
+        kpts = orb.detect(im,None)
+        
+        #kpts, des = des_ext.compute(im, kpts)
+        kp, des = orb.compute(im, kpts)
+        
+        des_list.append((image_path, des))
+        
+        
+    # Stack all the descriptors vertically in a numpy array
+    print 'Converting data to main matrix. . .'
+    descriptors = des_list[0][1]
+    for image_path, descriptor in des_list[1:]:
+        descriptors = np.vstack((descriptors, descriptor))  
+    # Perform k-means clustering
+    k = 100
+    voc, variance = kmeans(descriptors, k, 1) 
+
+    # Calculate the histogram of features
+    print 'Calculating histogram of features. . .'
+    im_features = np.zeros((len(image_paths_m), k), "float32")
+    for i in xrange(len(image_paths_m)):
+        words, distance = vq(des_list[i][1],voc)
+        for w in words:
+            im_features[i][w] += 1
+
+    # Perform Tf-Idf vectorization
+    nbr_occurences = np.sum( (im_features > 0) * 1, axis = 0)
+    idf = np.array(np.log((1.0*len(image_paths_m)+1) / (1.0*nbr_occurences + 1)), 'float32')
+
+    # Scaling the words
+    print 'Scaling Words. . .'
+    stdSlr = StandardScaler().fit(im_features)
+    im_features = stdSlr.transform(im_features)
+
+    return im_features
+
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
+                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+    """
+    Generate a simple plot of the test and traning learning curve.
+
+    Parameters
+    ----------
+    estimator : object type that implements the "fit" and "predict" methods
+        An object of that type which is cloned for each validation.
+
+    title : string
+        Title for the chart.
+
+    X : array-like, shape (n_samples, n_features)
+        Training vector, where n_samples is the number of samples and
+        n_features is the number of features.
+
+    y : array-like, shape (n_samples) or (n_samples, n_features), optional
+        Target relative to X for classification or regression;
+        None for unsupervised learning.
+
+    ylim : tuple, shape (ymin, ymax), optional
+        Defines minimum and maximum yvalues plotted.
+
+    cv : integer, cross-validation generator, optional
+        If an integer is passed, it is the number of folds (defaults to 3).
+        Specific cross-validation objects can be passed, see
+        sklearn.cross_validation module for the list of possible objects
+
+    n_jobs : integer, optional
+        Number of jobs to run in parallel (default 1).
+    """
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
+
+
+t0 = time()
 
 # Initate ORB detector object
 orb = cv2.ORB_create()
@@ -29,32 +144,29 @@ training_names = os.listdir(train_path)
 
 # Get all the path to the images and save them in a list
 # image_paths and the corresponding label in image_paths
-image_paths_tr = []
-image_classes_tr = []
+image_paths_m = []
+image_classes_m = []
 class_id = 0
 
-#####
-#features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(main, target, test_size = 0.2)
-####
 print 'Saving image paths as a list'
 for training_name in training_names:
     dir = os.path.join(train_path, training_name)
     class_path = imutils.imlist(dir)
-    image_paths_tr+=class_path
-    image_classes_tr+=[class_id]*len(class_path)
+    image_paths_m+=class_path
+    image_classes_m+=[class_id]*len(class_path)
     class_id+=1
     
-image_paths_tr, image_paths_te, image_classes_tr, image_classes_te = cross_validation.train_test_split(image_paths_tr, image_classes_tr, test_size = 0.2)
-
+image_paths_tr, image_paths_te, image_classes_tr, image_classes_te = cross_validation.train_test_split(image_paths_m, image_classes_m, test_size = 0.2)
 
 
 # List where all the descriptors are stored
 des_list = []
+
 print 'Iterating through features'
 for image_path in image_paths_tr:
     #print image_path
     im = cv2.imread(image_path)
-    im = imutilspy.resize(im, height = 400)
+    im = imutilspy.resize(im, height = 200)
     
     #kpts = fea_det.detect(im)
     kpts = orb.detect(im,None)
@@ -92,9 +204,11 @@ print 'Scaling Words. . .'
 stdSlr = StandardScaler().fit(im_features)
 im_features = stdSlr.transform(im_features)
 
+
 # Train the Linear SVM
 print 'Training SVM. . .'
-clf = LinearSVC()
+#clf = LinearSVC()
+clf = svm.SVC(kernel='poly')
 clf.fit(im_features, np.array(image_classes_tr))
 
 # Save the SVM
@@ -102,6 +216,8 @@ print 'Saving SVM model. . .'
 joblib.dump((clf, training_names, stdSlr, k, voc), "bof_cuav.pkl", compress=3)    
 print 'Done!'    
 
+total_time = time() - t0
+print total_time, 's'
 
 
 
@@ -111,8 +227,8 @@ print 'Done!'
 ############# Entering predicting phase ###############
 
 
-
-
+print 'Entering prediction phase. . .'
+t0 = time()
 
 # Load the classifier, class names, scaler, number of clusters and vocabulary 
 clf, classes_names, stdSlr, k, voc = joblib.load("bof_cuav.pkl")
@@ -171,10 +287,10 @@ des_list = []
 
 for image_path in image_paths_te:
     im = cv2.imread(image_path)
+    im = imutilspy.resize(im, height = 200)
     if im == None:
         print "No such file {}\nCheck if the file exists".format(image_path)
         exit()
-    im = imutilspy.resize(im, height = 400)
     #kpts = fea_det.detect(im)
     kpts = orb.detect(im,None)
     
@@ -210,23 +326,123 @@ predictions =  [classes_names[i] for i in clf.predict(test_features)]
 predictions_1 =  [class_num[i] for i in clf.predict(test_features)]
 
 # Visualize the results, if "visualize" flag set to true by the user
+
+accuracy = accuracy_score(image_classes_te, predictions_1)
+
+print 'Finished'
+total_time = time() - t0
+print total_time, 's'
+print ''
+print ''
+
+print 'Confusion Matrix: '
+print confusion_matrix(image_classes_te, predictions_1)
+
+# Accuracy in the 0.9333, 9.6667, 1.0 range
+print 'Score: ',accuracy
+
+
+while True:
+    print 'Visualize the classifications? [y] or [n]'
+    response = raw_input()
+    if response == 'y' or response == 'n':
+        if response == 'n':
+            visualize = False
+        break
+
 if visualize:
     for image_path, prediction in zip(image_paths_te, predictions):
         image = cv2.imread(image_path)
         cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
         pt = (0, 3 * image.shape[0] // 4)
         cv2.putText(image, prediction, pt ,cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 2, [0, 255, 0], 2)
+        image = imutilspy.resize(image, height = 400)
         cv2.imshow("Image", image)
         print '- ', prediction
         cv2.waitKey(3000)
 
 
 
-accuracy = accuracy_score(image_classes_te, predictions_1)
 
-print 'Confusion Matrix: '
-print confusion_matrix(image_classes_te, predictions_1)
 
-# Accuracy in the 0.9333, 9.6667, 1.0 range
-print accuracy
+print ''
+print ''
+print 'Press [p] to plot validation and learning curve or else press anything else: '
+plot_option = raw_input()
+
+if plot_option == 'p' or plot_option == 't':
+    t0 = time()
+    print 'Plotting validation learning curve. . .'
+    # SVM model
+    estimator = clf
+
+    
+    cv = ShuffleSplit(im_features.shape[0], n_iter=10, test_size=0.2, random_state=0)
+    gammas = np.logspace(-6, -1, 10)
+    classifier = GridSearchCV(estimator=estimator, cv=cv, param_grid=dict(gamma=gammas))
+    classifier.fit(im_features, image_classes_tr)
+    title = 'Learning Curves (SVM, linear kernel, $\gamma=%.6f$)' %classifier.best_estimator_.gamma
+    estimator = SVC(kernel='linear', gamma=classifier.best_estimator_.gamma)
+
+    if plot_option == 'p':
+        plot_learning_curve(estimator, title, im_features, np.array(image_classes_tr), cv=cv)
+        print 'Finished'
+        total_time = time() - t0
+        print total_time, 's'
+        plt.show()
+
+
+    t0 = time()    
+    #print 'Constructing main matrix. . .'
+    #main_mat = main_mat_construct(image_paths_m)
+    
+    print 'Plotting validation curve. . .'
+    param_range = np.logspace(-6, -1, 5)
+    '''
+    train_scores, test_scores = validation_curve(
+        SVC(), main_mat, image_classes_m, param_name="gamma", param_range=param_range,
+        cv=10, scoring="accuracy", n_jobs=1)
+    '''
+    train_scores, test_scores = validation_curve(
+        SVC(), im_features, np.array(image_classes_tr), param_name="gamma", param_range=param_range,
+        cv=10, scoring="accuracy", n_jobs=1)
+
+    
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    print 'Variables assigned'
+    print 'Preparing plot. . .'
+    plt.title("Validation Curve with SVM")
+    plt.xlabel("$\gamma$")
+    plt.ylabel("Score")
+    plt.ylim(0.0, 1.1)
+    plt.semilogx(param_range, train_scores_mean, label="Training score", color="r")
+    plt.fill_between(param_range, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.2, color="r")
+    plt.semilogx(param_range, test_scores_mean, label="Cross-validation score",
+                 color="g")
+    plt.fill_between(param_range, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.2, color="g")
+    plt.legend(loc="best")
+
+    if plot_option == 't':
+        plot_learning_curve(estimator, title, im_features, np.array(image_classes_tr), cv=cv)
+
+
+    print 'Finished'
+    total_time = time() - t0
+    print total_time, 's'
+    
+    plt.show()
+
+
+
+
+
+
+
+
+
 
